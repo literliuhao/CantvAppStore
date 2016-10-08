f479cf4e33c56f9747396685273f341ff408a2a6
kylinihaha <kylinhaha@gmail.com>
2016-10-08 15:57:32 +0800

    init

---
+++ app/src/main/java/com/can/appstore/MainActivity.java
@@ -0,0 +1,21 @@
+package com.can.appstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * ================================================
+ * 作    者：
+ * 版    本：
+ * 创建日期：
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/can/appstore/MyApp.java
@@ -0,0 +1,19 @@
+package com.can.appstore;
+
+import android.app.Application;
+
+/**
+ * ================================================
+ * 作    者：
+ * 版    本：
+ * 创建日期：
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+public class MyApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
