40863d8a02df2b7e0a354891b65a2359d4cd36e2
yibh <yibhgb@163.com>
2016-10-14 19:02:31 +0800

    搜索页利用模拟数据完成基本逻辑

---
+++ app/src/main/java/com/can/appstore/search/bean/DefaultApp.java
@@ -0,0 +1,15 @@
+package com.can.appstore.search.bean;
+
+/**
+ * Created by yibh on 2016/10/13 18:36 .
+ */
+
+public class DefaultApp {
+    public String mName;
+    public String mInitials;    //首字母
+
+    public DefaultApp(String mName, String mInitials) {
+        this.mName = mName;
+        this.mInitials = mInitials;
+    }
+}
