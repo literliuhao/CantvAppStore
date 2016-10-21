51d57a02efd16090aa5c91db3a0906cc1719396b
yibh <yibhgb@gmail.com>
2016-10-21 11:58:52 +0800

    排行榜模拟数据展示完成

--- app/src/main/java/com/can/appstore/homerank/HomeRankAdapter.java
+++ app/src/main/java/com/can/appstore/homerank/HomeRankAdapter.java
@@ -4,12 +4,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.can.appstore.R;
+import com.can.appstore.homerank.bean.AppInfo;
 
 import java.util.List;
 
 import cn.can.tvlib.ui.view.recyclerview.CanRecyclerViewAdapter;
+import cn.can.tvlib.utils.ToastUtils;
 
 /**
  * Created by yibh on 2016/10/20 17:11 .
@@ -17,26 +21,41 @@ import cn.can.tvlib.ui.view.recyclerview.CanRecyclerViewAdapter;
 
 public class HomeRankAdapter extends CanRecyclerViewAdapter {
     private List mList;
+
     public HomeRankAdapter(List datas) {
         super(datas);
         this.mList = datas;
     }
 
     @Override
-    protected RecyclerView.ViewHolder generateViewHolder(ViewGroup parent, int viewType) {
+    protected RecyclerView.ViewHolder generateViewHolder(final ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rank_list_item, parent, false);
+        setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onClick(View view, int position, Object data) {
+                ToastUtils.showMessage(parent.getContext(), ((AppInfo) mList.get(position)).getName());
+            }
+        });
         return new RankAppItemViewHolder(view);
     }
 
     @Override
     protected void bindContentData(Object mDatas, RecyclerView.ViewHolder holder, int position) {
-
+        if (holder instanceof RankAppItemViewHolder) {
+            AppInfo mApp = (AppInfo) mList.get(position);
+            ((RankAppItemViewHolder) holder).mAppName.setText(mApp.getName());
+        }
     }
 
-    class RankAppItemViewHolder extends RecyclerView.ViewHolder{
+    class RankAppItemViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mAppIcon;
+        private TextView mAppName;
 
         public RankAppItemViewHolder(View itemView) {
             super(itemView);
+            mAppIcon = (ImageView) itemView.findViewById(R.id.icon_view);
+            mAppName = (TextView) itemView.findViewById(R.id.app_name_view);
         }
     }
 
