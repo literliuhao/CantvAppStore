c99b3572ec3b0c3922c8f74041e14da728d7f2df
liuhao <liuhaoliter@gmail.com>
2016-10-31 11:55:21 +0800

    添加内涵段子，为你写代码带来无限乐趣！

--- app/src/main/java/com/can/appstore/MainActivity.java
+++ app/src/main/java/com/can/appstore/MainActivity.java
@@ -1,10 +1,16 @@
 package com.can.appstore;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
+import com.can.appstore.index.Data.JokeData;
+import com.can.appstore.index.IndexActivity;
 import com.can.appstore.search.SearchActivity;
+
+import java.util.Random;
 
 /**
  * ================================================
@@ -21,10 +27,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.bt_search).setOnClickListener(new View.OnClickListener() {
+        //随机获得内涵段子
+        int random = new Random().nextInt(5);
+
+        TextView textViewTitle = (TextView) findViewById(R.id.tv_joke_title);
+        textViewTitle.setText("内涵" + (random + 1) + "段：为你提神醒脑！");
+
+        TextView textView = (TextView) findViewById(R.id.tv_joke);
+        textView.setText(JokeData.getJokeList().get(random));
+
+        findViewById(R.id.btn_index).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SearchActivity.startAc(MainActivity.this);
+                startActivity(new Intent(MainActivity.this, IndexActivity.class));
+            }
+        });
+        findViewById(R.id.btn_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
             }
         });
     }
