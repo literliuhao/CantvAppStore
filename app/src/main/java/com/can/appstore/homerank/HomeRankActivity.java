62cc02d0346e192edfd8ec6760fe8b50fb3e31f9
yibh <yibhgb@gmail.com>
2016-11-18 14:24:43 +0800

    首页排行

--- app/src/main/java/com/can/appstore/homerank/HomeRankActivity.java
+++ app/src/main/java/com/can/appstore/homerank/HomeRankActivity.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.can.appstore.R;
+import com.can.appstore.index.interfaces.IAddFocusListener;
 
-public class HomeRankActivity extends AppCompatActivity {
+public class HomeRankActivity extends AppCompatActivity implements IAddFocusListener {
 
     private FrameLayout mFrameLayout;
     private static final String TAG = HomeRankActivity.class.getSimpleName();
@@ -24,12 +26,17 @@ public class HomeRankActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_rank);
         mFrameLayout = (FrameLayout) findViewById(R.id.rank_framelayout);
 
-//        if (null == savedInstanceState) {
-////            HomeRankFragment homeRankFragment = HomeRankFragment.newInstance("", "");
-//            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.rank_framelayout, homeRankFragment, TAG)
-//                    .commit();
-//        }
+        if (null == savedInstanceState) {
+            HomeRankFragment homeRankFragment = new HomeRankFragment(this);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.rank_framelayout, homeRankFragment, TAG)
+                    .commit();
+        }
+
+    }
+
+    @Override
+    public void addFocusListener(View v, boolean hasFocus) {
 
     }
 }
