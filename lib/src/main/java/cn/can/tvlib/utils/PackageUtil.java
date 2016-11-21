a801af0d0489d15c9fb9d60ff0f42fc548c700da
wmf <18539685929@163.com>
2016-11-21 11:15:23 +0800

    工具类过滤黑名单

--- lib/src/main/java/cn/can/tvlib/utils/PackageUtil.java
+++ lib/src/main/java/cn/can/tvlib/utils/PackageUtil.java
@@ -413,10 +413,70 @@ public class PackageUtil {
             }
         }
         return appList;
+    } /**
+     * 获取已安装的第三方应用信息 - 黑名单
+     *
+     * @param context
+     * @return
+     */
+    public static List<AppInfo> findAllexBllackThirdApps(Context context, List<AppInfo> appList, List<String> appBlackList) {
+        if (appList == null) {
+            appList = new ArrayList<>();
+        } else {
+            appList.clear();
+        }
+        PackageManager pm = context.getApplicationContext().getPackageManager();
+        List<PackageInfo> pList = pm.getInstalledPackages(0);
+        final AtomicInteger index = new AtomicInteger();
+        for (PackageInfo info : pList) {
+            ApplicationInfo applicationInfo = info.applicationInfo;
+            // 是否是系统权限
+            boolean isSystemApp = (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == ApplicationInfo.FLAG_SYSTEM;
+            if (isSystemApp) {
+                continue;
+            }
+            if(appBlackList.contains(info.packageName)){
+                continue;
+            }
+            index.incrementAndGet();
+            final AppInfo app = new AppInfo();
+            app.isSystemApp = isSystemApp;
+            app.packageName = info.packageName;
+            app.versionName = info.versionName;
+            app.versionCode = info.versionCode;
+            app.installtime = info.firstInstallTime;
+            app.installPath = applicationInfo.sourceDir;
+            app.appIcon = applicationInfo.loadIcon(pm);
+            app.appName = applicationInfo.loadLabel(pm).toString();
+            // 应用占用空间大小
+            try {
+                Method method = PackageManager.class.getMethod("getPackageSizeInfo", String.class, IPackageStatsObserver.class);
+                method.setAccessible(true);
+                method.invoke(pm, app.packageName, new IPackageStatsObserver.Stub() {
+                    @Override
+                    public void onGetStatsCompleted(PackageStats pStats, boolean succeeded) throws RemoteException {
+                        app.size = pStats.codeSize + pStats.dataSize + pStats.cacheSize;
+                        int i = index.decrementAndGet();
+                    }
+                });
+            } catch (Exception e) {
+                File apk = new File(applicationInfo.sourceDir);
+                app.size = apk.length();// apk包文件大小
+            }
+            appList.add(app);
+        }
+        while (index.get() > 0) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return appList;
     }
     /**
      * 获取处于白名单中的系统应用 + 第三方应用 - 黑名单
-     * TODO
+     *
      * //逻辑有问题
      * @param context
      * @return
@@ -434,7 +494,10 @@ public class PackageUtil {
             ApplicationInfo applicationInfo = info.applicationInfo;
             boolean isSystemApp = (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == ApplicationInfo.FLAG_SYSTEM;
             // 排除不在白名单中的系统应用
-            if (isSystemApp && !appWhiteList.contains(info.packageName) && appBlackList.contains(info.packageName)) {
+            if (isSystemApp && !appWhiteList.contains(info.packageName)) {
+                continue;
+            }
+            if(appBlackList.contains(info.packageName)){
                 continue;
             }
             index.incrementAndGet();
