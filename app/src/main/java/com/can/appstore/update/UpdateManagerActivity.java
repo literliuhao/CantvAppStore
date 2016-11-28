f21151dc3a79cb9edfa05707c214a968ed340a4c
shenpx <xp11235813>
2016-11-28 16:07:36 +0800

    fix bug

--- app/src/main/java/com/can/appstore/update/UpdateManagerActivity.java
+++ app/src/main/java/com/can/appstore/update/UpdateManagerActivity.java
@@ -243,8 +243,6 @@ public class UpdateManagerActivity extends Activity implements UpdateContract.Vi
                     return;
                 } else {
                     mPresenter.getInstallPkgList(mAutoUpdate);
-                    mCurrentnum.setVisibility(View.VISIBLE);
-                    mTotalnum.setVisibility(View.VISIBLE);
                     if (!NetworkUtils.isNetworkConnected(UpdateManagerActivity.this)) {
                         mCurrentnum.setVisibility(View.INVISIBLE);
                         mTotalnum.setVisibility(View.INVISIBLE);
@@ -557,6 +555,7 @@ public class UpdateManagerActivity extends Activity implements UpdateContract.Vi
 
     @Override
     public void showNoData() {
+        mReminder.setText(getResources().getString(R.string.update_updateall));
         mReminder.setVisibility(View.VISIBLE);
         mCurrentnum.setVisibility(View.INVISIBLE);
         mTotalnum.setVisibility(View.INVISIBLE);
