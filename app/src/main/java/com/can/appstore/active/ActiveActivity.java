50778033dcc3ae9863cc79ce43063a3d55b1cdaf
fuwen <fuwen@cantv.cn>
2016-11-29 18:14:18 +0800

    活动页还原下载逻辑（由single改为普通下载）

--- app/src/main/java/com/can/appstore/active/ActiveActivity.java
+++ app/src/main/java/com/can/appstore/active/ActiveActivity.java
@@ -1,24 +1,15 @@
 package com.can.appstore.active;
 
-import android.app.Activity;
 import android.app.Dialog;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.Message;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -28,45 +19,19 @@ import android.widget.RelativeLayout;
 
 import com.can.appstore.R;
 import com.can.appstore.base.BaseActivity;
-import com.can.appstore.entity.AppInfo;
-import com.can.appstore.entity.Result;
-import com.can.appstore.http.CanCall;
-import com.can.appstore.http.CanCallback;
-import com.can.appstore.http.CanErrorWrapper;
-import com.can.appstore.http.HttpManager;
 import com.can.appstore.search.ToastUtil;
 import com.can.appstore.widgets.TextProgressBar;
 
-import cn.can.downloadlib.AppInstallListener;
-import cn.can.downloadlib.DownloadManager;
-import cn.can.downloadlib.DownloadStatus;
-import cn.can.downloadlib.DownloadTask;
-import cn.can.downloadlib.DownloadTaskListener;
-import cn.can.downloadlib.MD5;
 import cn.can.downloadlib.NetworkUtils;
-import cn.can.downloadlib.utils.SdcardUtils;
-import cn.can.downloadlib.utils.ShellUtils;
-import cn.can.downloadlib.utils.ToastUtils;
 import cn.can.tvlib.imageloader.ImageLoader;
-import cn.can.tvlib.ui.focus.FocusMoveUtil;
-import cn.can.tvlib.ui.widgets.LoadingDialog;
-import cn.can.tvlib.utils.ApkUtils;
-import cn.can.tvlib.utils.MD5Util;
-import cn.can.tvlib.utils.PackageUtil;
 import cn.can.tvlib.utils.PromptUtils;
-import cn.can.tvlib.utils.StringUtils;
-import retrofit2.Response;
-
-import static com.can.appstore.MyApp.mContext;
-import static com.can.appstore.R.string.active_app_installing;
 
 /**
  * Created by Atangs on 2016/11/1.
  * 活动页
  */
 
-public class ActiveActivity extends BaseActivity implements View.OnClickListener {
-    private final static String TAG = "ActivePresenter";
+public class ActiveActivity extends BaseActivity implements ActiveContract.OperationView, View.OnClickListener {
     public static final String EXTRA_ACTIVE_ID = "activeId";
     private final static int ACTIVE_PARTICIPATE = R.mipmap.active_participate;
     private final static int ACTIVE_NORMAL = R.mipmap.active_normal;
@@ -74,20 +39,16 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
     private final static int REFRESH_PROGRESSBAR_PROGRESS = 0x11;
     private final static int SHOW_TOAST = 0x12;
 
-    private AppInstallService mInstallService;
     private WebView mActiveWebview;
     private TextProgressBar mActiveTextProgressBar;
     private RelativeLayout mActiveLayout;
     private RelativeLayout mNetworkLayout;
     private Button mRetryBtn;
     private ImageView mImgBg;
+    private ActivePresenter mActivePresenter;
     private String mActiveId;
     private int mShowType;
     private LinearLayout mFocusLayout;
-
-    private CanCall<Result<com.can.appstore.entity.Activity>> mActiveData;
-    private AppInfo mAppInfo;
-
 
     public static void actionStart(Context context, String activeId) {
         Intent intent = new Intent(context, ActiveActivity.class);
@@ -105,9 +66,8 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
         }
         mActiveId = TextUtils.isEmpty(mActiveId) ? "52" : mActiveId;
         initUI();
-
-        this.bindService(new Intent(ActiveActivity.this,AppInstallService.class), mInstallServiceConnection,
-                BIND_AUTO_CREATE);
+        mActivePresenter = new ActivePresenter(this, ActiveActivity.this);
+        mActivePresenter.requestActiveData(mActiveId);
     }
 
     private void initUI() {
@@ -121,61 +81,11 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
         mActiveTextProgressBar.setTextSize(R.dimen.px40);
         mActiveTextProgressBar.setTextFakeBoldText(true);
         mActiveTextProgressBar.setTextColor(Color.WHITE);
-        mActiveTextProgressBar.setText(getString(R.string.active_click_participate));
         mActiveTextProgressBar.setOnClickListener(this);
         mRetryBtn.setOnClickListener(this);
     }
 
-    private ServiceConnection mInstallServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-
-            mInstallService = ((AppInstallService.InstallBinder) service).getInstallService();
-
-            ((AppInstallService.InstallBinder) service).setActivity(ActiveActivity.this);
-            requestActiveData(mActiveId);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            mInstallService = null;
-        }
-    };
-
-    public void requestActiveData(String activeId) {
-        mActiveData = HttpManager.getApiService().getActivityInfo(activeId);
-        mActiveData.enqueue(new CanCallback<Result<com.can.appstore.entity.Activity>>() {
-            @Override
-            public void onResponse(CanCall<Result<com.can.appstore.entity.Activity>> call, Response<Result<com.can.appstore.entity.Activity>> response) throws Exception {
-                Result<com.can.appstore.entity.Activity> info = response.body();
-                if (info == null) {
-                    showNetworkRetryView(true, false);
-                    return;
-                }
-                if (info.getData() == null) {
-                    return;
-                }
-                com.can.appstore.entity.Activity active = info.getData();
-                boolean isWebView = StringUtils.isEmpty(active.getUrl());
-                showNetworkRetryView(false, isWebView);
-                if (isWebView) {
-                    mAppInfo = active.getRecommend();
-                    setNativeLayout(active.getBackground());
-                    mInstallService.initDownloadTask(mAppInfo.getUrl());
-                } else {
-                    loadwebview(active.getUrl());
-                }
-            }
-
-            @Override
-            public void onFailure(CanCall<Result<com.can.appstore.entity.Activity>> call, CanErrorWrapper errorWrapper) {
-                if (!cn.can.tvlib.utils.NetworkUtils.isNetworkConnected(mContext.getApplicationContext())) {
-                    showNetworkRetryView(true, false);
-                }
-            }
-        });
-    }
-
+    @Override
     public void refreshProgressbarProgress(float progress) {
         mHandler.removeMessages(REFRESH_PROGRESSBAR_PROGRESS);
         Message msg = mHandler.obtainMessage();
@@ -184,6 +94,7 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
         mHandler.sendMessage(msg);
     }
 
+    @Override
     public void refreshTextProgressbarTextStatus(int status) {
         mHandler.removeMessages(REFRESH_PROGRESSBAR_TEXT);
         Message msg = mHandler.obtainMessage();
@@ -192,25 +103,16 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
         mHandler.sendMessage(msg);
     }
 
-    private void setTextProgressbarText(int status) {
-        Log.d(TAG, getString(status));
-        int curType = status == R.string.active_click_participate ? ACTIVE_PARTICIPATE : ACTIVE_NORMAL;
-        if (curType != mShowType) {
-            mFocusLayout.setBackgroundResource(curType);
-            mShowType = curType;
-        }
-        mActiveTextProgressBar.setText(getString(status));
-    }
-
     @Override
-    public void showToast(int toastStrId) {
+    public void showToast(String toastContent) {
         mHandler.removeMessages(SHOW_TOAST);
         Message msg = mHandler.obtainMessage();
         msg.what = SHOW_TOAST;
-        msg.arg1 = toastStrId;
+        msg.obj = toastContent;
         mHandler.sendMessage(msg);
     }
 
+    @Override
     public void loadwebview(String url) {
         mActiveWebview.setWebViewClient(new CanWebViewClient());
         mActiveWebview.setVisibility(View.VISIBLE);
@@ -222,17 +124,19 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
      *
      * @param url
      */
+    @Override
     public void setNativeLayout(String url) {
         mActiveLayout.setVisibility(View.VISIBLE);
         mActiveTextProgressBar.requestFocus();
         ImageLoader.getInstance().load(ActiveActivity.this, mImgBg, url);
     }
 
+    @Override
     public void showNetworkRetryView(boolean isRetry, boolean isWebView) {
         mActiveWebview.setVisibility(!isRetry && isWebView ? View.VISIBLE : View.GONE);
         mActiveLayout.setVisibility(!isRetry && !isWebView ? View.VISIBLE : View.GONE);
         mNetworkLayout.setVisibility(isRetry ? View.VISIBLE : View.GONE);
-        if (isRetry) {
+        if(isRetry){
             mRetryBtn.requestFocus();
         }
     }
@@ -242,13 +146,20 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case REFRESH_PROGRESSBAR_TEXT:
-                    setTextProgressbarText(msg.arg1);
+                    int status = msg.arg1;
+                    if(status == R.string.active_click_participate){
+                        mShowType = ACTIVE_PARTICIPATE;
+                    }else {
+                        mShowType = ACTIVE_NORMAL;
+                    }
+                    mFocusLayout.setBackgroundResource(mShowType);
+                    mActiveTextProgressBar.setText(getString(status));
                     break;
                 case REFRESH_PROGRESSBAR_PROGRESS:
                     mActiveTextProgressBar.setProgress(msg.arg1);
                     break;
                 case SHOW_TOAST:
-                    ToastUtil.toastShort(getString(msg.arg1));
+                    ToastUtil.toastShort((String) msg.obj);
                     break;
             }
         }
@@ -258,14 +169,14 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.active_textprogressbar:
-                mInstallService.clickBtnDownload(mAppInfo);
+                mActivePresenter.clickBtnDownload();
                 break;
             case R.id.network_retry_btn:
                 if (!NetworkUtils.isNetworkConnected(ActiveActivity.this.getApplicationContext())) {
                     showToast(R.string.network_connection_disconnect);
                     return;
                 }
-                requestActiveData(mActiveId);
+                mActivePresenter.requestActiveData(mActiveId);
                 break;
         }
     }
@@ -297,18 +208,11 @@ public class ActiveActivity extends BaseActivity implements View.OnClickListener
 
     @Override
     protected void onStop() {
-        super.onStop();
-        if (mActiveData != null && !mActiveData.isCanceled()) {
-            mActiveData.cancel();
-            mActiveData = null;
+        if (mActivePresenter != null) {
+            mActivePresenter.release();
+            mActivePresenter = null;
         }
-
         mHandler.removeCallbacksAndMessages(null);
-    }
-
-    @Override
-    protected void onDestroy() {
-        unbindService(mInstallServiceConnection);
-        super.onDestroy();
+        super.onStop();
     }
 }
