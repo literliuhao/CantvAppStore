6fa47a678b64633596444ff0636f4b2810161404
“hekang <351033833@qq.com>
2016-12-05 16:21:36 +0800

    将请求来的消息数据处理放在子线程中

--- app/src/main/java/com/can/appstore/message/manager/MessageManager.java
+++ app/src/main/java/com/can/appstore/message/manager/MessageManager.java
@@ -79,61 +79,50 @@ public class MessageManager {
      * 请求服务器消息数据
      */
     public static void requestMsg(final Context context) {
-//        //假数据
-//        MessageManager.deleteExceedMsg(context, System.currentTimeMillis() / 1000);
-//        List<MessageInfo> list = new ArrayList<>();
-//        for (int i = 0; i < 10; i++) {
-//            MessageInfo msgInfo = new MessageInfo();
-//            msgInfo.setMsgId(System.currentTimeMillis() + i + "");
-//            msgInfo.setMsgExpires(System.currentTimeMillis() / 1000 + 600);
-//            msgInfo.setMsgTitle("【活动专区】下载有礼，下载有礼，下载有礼，下载有礼下载有礼，下载有礼，下载有礼，下载有礼");
-//            msgInfo.setStatus(true);
-//            msgInfo.setAction("action_nothing");
-//            msgInfo.setActionData("0");
-//            msgInfo.setMsgDate("2016-11-28");
-//            list.add(msgInfo);
-//        }
-//        MessageManager.insert(context, list);
-
         if (NetworkUtils.isNetworkConnected(context)) {
             CanCall<Result<MessageContainer>> mMessageContainer = HttpManager.getApiService().getMessages();
             mMessageContainer.enqueue(new CanCallback<Result<MessageContainer>>() {
                 @Override
-                public void onResponse(CanCall<Result<MessageContainer>> call, Response<Result<MessageContainer>> response) throws Exception {
-                    Result<MessageContainer> body = response.body();
-                    MessageContainer msgContainer = body.getData();
-                    if (msgContainer == null) {
-                        Log.w(TAG, "message data error");
-                        return;
-                    }
-                    long timestamp = msgContainer.getTimestamp();
-                    MessageManager.deleteExceedMsg(context, timestamp); // 删除过期数据
-                    List<Message> list = msgContainer.getMessages();
-                    if (list != null && !list.isEmpty()) {
-                        List<MessageInfo> msgList = new ArrayList<>();
-                        int size = list.size();
-                        for (int i = 0; i < size; i++) {
-                            Message msg = list.get(i);
-                            MessageInfo msgInfo = new MessageInfo();
-                            msgInfo.setMsgId(msg.getId() + System.currentTimeMillis());
-                            msgInfo.setMsgDate(msg.getDate());
-                            msgInfo.setMsgExpires(msg.getExpires());
-                            msgInfo.setMsgTitle(msg.getTitle());
-                            msgInfo.setStatus(true);
-                            //接口数据异常处理
-                            if (TextUtils.isEmpty(msg.getAction()) || TextUtils.isEmpty(msg.getActionData())) {
-                                msgInfo.setAction(ActionConstants.ACTION_NOTHIN);
-                            } else {
-                                msgInfo.setAction(msg.getAction().trim());  //处理数据首尾空格（经测试会出现首尾有空格情况）
-                                msgInfo.setActionData(msg.getActionData());
+                public void onResponse(CanCall<Result<MessageContainer>> call, final Response<Result<MessageContainer>> response) throws Exception {
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            Result<MessageContainer> body = response.body();
+                            MessageContainer msgContainer = body.getData();
+                            if (msgContainer == null) {
+                                Log.w(TAG, "message data error");
+                                return;
                             }
-                            msgInfo.setUserId(NetworkUtils.getMac());
-                            msgList.add(msgInfo);
+                            long timestamp = msgContainer.getTimestamp();
+                            MessageManager.deleteExceedMsg(context, timestamp); // 删除过期数据
+                            List<Message> list = msgContainer.getMessages();
+                            if (list != null && !list.isEmpty()) {
+                                List<MessageInfo> msgList = new ArrayList<>();
+                                int size = list.size();
+                                for (int i = 0; i < size; i++) {
+                                    Message msg = list.get(i);
+                                    MessageInfo msgInfo = new MessageInfo();
+                                    msgInfo.setMsgId(msg.getId() + System.currentTimeMillis());
+                                    msgInfo.setMsgDate(msg.getDate());
+                                    msgInfo.setMsgExpires(msg.getExpires());
+                                    msgInfo.setMsgTitle(msg.getTitle());
+                                    msgInfo.setStatus(true);
+                                    //接口数据异常处理
+                                    if (TextUtils.isEmpty(msg.getAction()) || TextUtils.isEmpty(msg.getActionData())) {
+                                        msgInfo.setAction(ActionConstants.ACTION_NOTHIN);
+                                    } else {
+                                        msgInfo.setAction(msg.getAction().trim());  //处理数据首尾空格（经测试会出现首尾有空格情况）
+                                        msgInfo.setActionData(msg.getActionData());
+                                    }
+                                    msgInfo.setUserId(NetworkUtils.getMac());
+                                    msgList.add(msgInfo);
+                                }
+                                MessageManager.insert(context, msgList); // 插入数据库
+                            } else {
+                                Log.i(TAG, "no new message");
+                            }
                         }
-                        MessageManager.insert(context, msgList); // 插入数据库
-                    } else {
-                        Log.i(TAG, "no new message");
-                    }
+                    }.start();
                 }
 
                 @Override
