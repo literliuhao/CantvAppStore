a4c0d37925274bafc090ab308cd2ee4b3662fba3
zhangbingyuan <1162617922@qq.com>
2016-12-08 16:14:26 +0800

    恢复通用请求参数-。-！

--- app/src/main/java/com/can/appstore/http/RequestParamsInterceptor.java
+++ app/src/main/java/com/can/appstore/http/RequestParamsInterceptor.java
@@ -1,6 +1,8 @@
 package com.can.appstore.http;
 
 import com.can.appstore.AppConstants;
+import com.can.appstore.BuildConfig;
+import com.can.appstore.entity.TvInfoModel;
 
 import java.io.IOException;
 
@@ -20,15 +22,15 @@ public class RequestParamsInterceptor implements Interceptor {
             HttpUrl url = request.url()
                     .newBuilder()
                     // Test -->
-                    .addQueryParameter("channelId", "12")
-                    .addQueryParameter("internalModel", "neibu")
-                    .addQueryParameter("model", "f66")
-                    .addQueryParameter("versionId", "1")
+//                    .addQueryParameter("channelId", "12")
+//                    .addQueryParameter("internalModel", "neibu")
+//                    .addQueryParameter("model", "f66")
+//                    .addQueryParameter("versionId", "1")
                     // <-- Test
-//                    .addQueryParameter("channelId", TvInfoModel.getInstance().getChannelId())
-//                    .addQueryParameter("internalModel", TvInfoModel.getInstance().getInternalmodelName())
-//                    .addQueryParameter("model", TvInfoModel.getInstance().getModelName())
-//                    .addQueryParameter("versionId", String.valueOf(BuildConfig.VERSION_CODE))
+                    .addQueryParameter("channelId", TvInfoModel.getInstance().getChannelId())
+                    .addQueryParameter("internalModel", TvInfoModel.getInstance().getInternalmodelName())
+                    .addQueryParameter("model", TvInfoModel.getInstance().getModelName())
+                    .addQueryParameter("versionId", String.valueOf(BuildConfig.VERSION_CODE))
                     .addQueryParameter("mac", NetworkUtils.getMac())
                     .build();
             request = request.newBuilder().url(url)
