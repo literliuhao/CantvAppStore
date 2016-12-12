ebccace3bbb8ee10a5e2463b09a850d1a08e9355
fanjianming <jianming0814@163.com>
2016-12-12 20:35:20 +0800

    去除多余日志

--- app/src/main/java/com/can/appstore/appdetail/adapter/IntroducGridAdapter.java
+++ app/src/main/java/com/can/appstore/appdetail/adapter/IntroducGridAdapter.java
@@ -40,7 +40,6 @@ public class IntroducGridAdapter extends CanRecyclerViewAdapter {
 
     @Override
     protected void bindContentData(Object mDatas, RecyclerView.ViewHolder holder, int position) {
-        Log.d("fjm", "bindContentData: ");
         final IntroducGridViewHolder introducGridViewHolder = (IntroducGridViewHolder) holder;
         introducGridViewHolder.introducItem.load(mIntroduceList.get(position), R.drawable.shap_image_bg,
                 R.mipmap.cibn_picture, R.mipmap.icon_loading_fail, true);
