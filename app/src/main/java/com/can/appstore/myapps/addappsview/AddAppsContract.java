eb25ba05c83b5c04f8ec9ad875db2b347c132e63
fanjianming <jianming0814@163.com>
2017-01-11 18:58:22 +0800

    添加应用页面appinfo修改

--- app/src/main/java/com/can/appstore/myapps/addappsview/AddAppsContract.java
+++ app/src/main/java/com/can/appstore/myapps/addappsview/AddAppsContract.java
@@ -3,6 +3,7 @@ package com.can.appstore.myapps.addappsview;
 
 import com.can.appstore.base.BasePresenter;
 import com.can.appstore.base.BaseView;
+import com.can.appstore.entity.SelectedAppInfo;
 
 import java.util.List;
 
@@ -24,11 +25,11 @@ public interface AddAppsContract {
 
     interface View extends BaseView{
 
-        void loadAddAppInfoSuccess(List<PackageUtil.AppInfo> infoList);
+        void loadAddAppInfoSuccess(List<SelectedAppInfo> infoList);
 
         void showCanSelectCount(int cansel, int alreadyshow);
 
-        void saveSelectInfo(List<PackageUtil.AppInfo> list);
+        void saveSelectInfo(List<SelectedAppInfo> list);
 
         void setAlreadySelectApp(int[] alreadySelect);
     }
